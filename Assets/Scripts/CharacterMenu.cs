bc8b69cf5be3abf9248a9134f8fc9c4c07d78ec4
Joe <joefrizzell@gmail.com>
2019-11-28 20:52:25 -0500

    Fleshing out Character Creation Menu

---
+++ Assets/Scripts/CharacterMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+public class CharacterMenu : MonoBehaviour
+{
+    [SerializeField] TMP_InputField nameInput;
+    [SerializeField] GameObject nameDialog;
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void CreateCharacter()
+    {
+        if(nameInput.text == "")
+        {
+            nameDialog.SetActive(true);
+            nameDialog.GetComponentInChildren<Button>().Select();
+        }
+        else
+        {
+            Debug.Log("TODO Save info to text file"); // TODO Save info to text file
+            SceneManager.LoadScene("StartingArea");
+        }
+    }
+}
