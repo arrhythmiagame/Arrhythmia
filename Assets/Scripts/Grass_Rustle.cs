3d4ce6489dd2e7dc85f0ce8917007ff321d1285f
Joe <joefrizzell@gmail.com>
2020-01-07 16:18:36 -0500

    adding long grass

---
+++ Assets/Scripts/Grass_Rustle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass_Rustle : MonoBehaviour
+{
+    [SerializeField] AudioClip grassSound;
+    private SpriteRenderer theSprite;
+    private void Start()
+    {
+        theSprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "Character")
+        {
+            theSprite.flipX = true;
+            AudioSource.PlayClipAtPoint(grassSound, transform.position);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.name == "Character")
+        {
+            theSprite.flipX = false;
+        }
+    }
+}
