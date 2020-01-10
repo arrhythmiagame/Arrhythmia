b1e4354ff8d37086c54fbd3043d42033dfc3c94d
Joe <joefrizzell@gmail.com>
2020-01-10 15:00:48 -0500

    improving grass display

--- Assets/Scripts/Grass_Rustle.cs
+++ Assets/Scripts/Grass_Rustle.cs
@@ -6,18 +6,22 @@ public class Grass_Rustle : MonoBehaviour
 {
     [SerializeField] AudioClip grassSound;
     [SerializeField] Sprite grassDown;
+    [SerializeField] bool reversed;
     private Sprite grassUp;
     private SpriteRenderer theSprite;
     private void Start()
     {
+        reversed = (Random.value > 0.5f);
         theSprite = gameObject.GetComponent<SpriteRenderer>();
         grassUp = theSprite.sprite;
+        theSprite.flipX = reversed;
     }
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.name == "Character")
         {
-            theSprite.flipX = true;
+            theSprite.flipX = reversed;
+            reversed = !reversed;
             theSprite.sprite = grassDown;
             AudioSource.PlayClipAtPoint(grassSound, transform.position);
         }
@@ -26,7 +30,8 @@ public class Grass_Rustle : MonoBehaviour
     {
         if (collider.name == "Character")
         {
-            theSprite.flipX = false;
+            theSprite.flipX = reversed;
+            reversed = !reversed;
             theSprite.sprite = grassUp;
         }
     }
